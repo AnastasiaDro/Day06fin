b8ccd08d1e33d55fc3b8a7ecb4554d0e9f1fa172
cerebus <cerebus@student.21-school.ru>
2021-10-19 18:01:52 +0300

    -added utils file for my own utils funcs -added my lovely printMsg to Utils.*

---
+++ Utils.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Cesar Erebus on 10/19/21.
+//
+
+#include "Utils.hpp"
+
+void printMsg(std::string &msg)
+{
+	std::cout << msg << std::endl;
+}
---
+++ Utils.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Cesar Erebus on 10/19/21.
+//
+
+#ifndef DAY06_UTILS_HPP
+#define DAY06_UTILS_HPP
+
+
+#include <string>
+#include <iostream>
+
+void printMsg(std::string &msg);
+
+
+#endif //DAY06_UTILS_HPP
